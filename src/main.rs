4ba4e1ca1b4c4749ca8ee8b70abda61bf5d6b1a2
tcarrio <tom@carrio.me>
2016-11-08 15:15:51 -0500

    Working XDG_CACHE_HOME retrieval

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::env;
+
 fn main(){
-	println!("{}",get_config_dirs());
+	println!("{}",get_xdg_config());
 }
 
-fn get_config_dirs() -> String {
-	"Hey there!".to_string()
+fn get_xdg_config() -> String {
+	match env::var("XDG_CACHE_HOME".to_string()){
+		Ok(val) => val.to_string(),
+		Err(e) => get_default_config(),
+	}
+}
+
+fn get_default_config() -> String {
+	match env::var("HOME".to_string()){
+		Ok(val) => val.to_string()+"/.cache/clm",
+		Err(e) => "Could not be retrieved".to_string(),
+	}
 }
